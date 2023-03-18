44a80e52597864b4aef08e90e2af6758b72690dc
N. Pattakos <pattakosn@fastmail.com>
2023-03-19 00:19:37 +0100

    wip: toying with simd: fix linux+msvc build

---
+++ src/my_cpuid.h
@@ -0,0 +1,40 @@
+#ifndef BENCHMARKING_TOY_CODES_MY_CPUID_H
+#define BENCHMARKING_TOY_CODES_MY_CPUID_H
+#include <string>
+#include <iostream>
+
+//NIKOS from https://stackoverflow.com/questions/1666093/cpuid-implementations-in-c
+#ifdef _WIN32
+#include <limits.h>
+#include <intrin.h>
+typedef unsigned __int32  uint32_t;
+#else
+#include <cstdint>
+#endif
+class MY_CPUID {
+        uint32_t regs[4] = {0};
+public:
+        explicit MY_CPUID(unsigned i) {
+#ifdef _WIN32
+                __cpuid((int *)regs, (int)i);
+//                _asm
+//                {
+//                  cpuid;
+//                  mov regs[0], ebx;
+//                  mov regs[4], edx;
+//                  mov regs[8], ecx;
+//                }
+//                return string((const char*)data);
+#else
+                asm volatile
+                        ("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
+                        : "a" (i), "c" (0));
+                // ECX is set to zero for MY_CPUID function 4
+#endif
+        }
+        const uint32_t &EAX() const {return regs[0];}
+        const uint32_t &EBX() const {return regs[1];}
+        const uint32_t &ECX() const {return regs[2];}
+        const uint32_t &EDX() const {return regs[3];}
+};
+#endif
--- src/simd.cpp
+++ src/simd.cpp
@@ -1,41 +1,12 @@
-#include <benchmark/benchmark.h>
-#include <string>
-#include <iostream>
-
-
-//NIKOS from https://stackoverflow.com/questions/1666093/cpuid-implementations-in-c
-//#ifndef CPUID_H
-//#define CPUID_H
-#ifdef _WIN32
-#include <limits.h>
-#include <intrin.h>
-typedef unsigned __int32  uint32_t;
-#else
-#include <stdint.h>
-#endif
-class CPUID {
-        uint32_t regs[4];
-public:
-        explicit CPUID(unsigned i) {
-#ifdef _WIN32
-                __cpuid((int *)regs, (int)i);
-#else
-                asm volatile
-                        ("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
-                        : "a" (i), "c" (0));
-                // ECX is set to zero for CPUID function 4
-#endif
-        }
-        const uint32_t &EAX() const {return regs[0];}
-        const uint32_t &EBX() const {return regs[1];}
-        const uint32_t &ECX() const {return regs[2];}
-        const uint32_t &EDX() const {return regs[3];}
-};
-//#endif // CPUID_H
+//#include <benchmark/benchmark.h>
+#include <array>
+#include <cpuid.h>
+#include "my_cpuid.h"
+#include "immintrin.h"
 
 static std::string get_cpu_name()
 {
-        CPUID cpuID(0); // Get CPU vendor
+        MY_CPUID cpuID(0); // Get CPU vendor
 
         std::string vendor;
         vendor += std::string((const char *)&cpuID.EBX(), 4);
@@ -43,11 +14,72 @@ static std::string get_cpu_name()
         vendor += std::string((const char *)&cpuID.ECX(), 4);
         return vendor;
 }
-static void inline_assembly(benchmark::State &state) {
-        std::cout <<"cpu found: " << get_cpu_name() << std::endl;
-        for (auto _ : state)
-                ;
-}
-BENCHMARK(inline_assembly);
+//static void inline_assembly(benchmark::State &state) {
+//        std::cout <<"cpu found: " << get_cpu_name() << std::endl;
+//        for (auto _ : state)
+//                ;
+//}
+//BENCHMARK(inline_assembly);
+//
+//BENCHMARK_MAIN();
 
-BENCHMARK_MAIN();
+void multiply_vectors() {
+        float f1[] = {1., 2., 3., 4.};
+        float f2[] = {4., 3., 2., 1.};
+        std::array res{0.f, 0.f, 0.f, 0.f};
+        _asm
+        {
+                movups xmm1, f1;
+                movups xmm2. f2;
+                mulps xmm1, xmm2;
+                movups res, xmm1;
+        }
+        std::cout << std::endl;
+        for(size_t i = 0; i < res.size(); ++i)
+                std::cout << "res[ " << i << " ]= " << res[i] << std::endl;
+        std::cout << std::endl;
+}
+
+void report_sse_support() {
+        int d, c;
+        _asm
+        {
+                mov eax, 1;
+                cpuid;
+                mov d, edx;
+                mov c, ecx;
+        }
+        if((d & (1<<26)) != 0)
+                std::cout << "SSE2 supported" << std::endl;
+        if((c & 1) != 0)
+                std::cout << "SSE3 supported" << std::endl;
+        if((c & (1<<19)) != 0)
+                std::cout << "SSE4.1 supported" << std::endl;
+        if((c & (1<<20)) != 0)
+                std::cout << "SSE4.2 supported" << std::endl;
+}
+
+std::string get_cpu_name_intrinsics() {
+        //int cpuidInfo[4];
+        //__cpuid(cpuIdInfo, 1);
+        auto a = _mm_set_ps(1., 2., 3., 4.);
+        auto b = _mm_set_ps(4, 3, 2, 1);
+        auto c = _mm_add_ps(a, b);
+        //auto res = c.m128_f32[0];
+        __try {
+                __m256 z = _mm256_set_pd(1., 2., 3., 4.);
+        } __except (1) {
+                std::cout << "sorry, not sorry" <<std::endl;
+        }
+}
+int main(int, char*[]) {
+        std::cout <<"cpu found: " << get_cpu_name() << std::endl;
+
+        multiply_vectors();
+
+        report_sse_support();
+
+        std::cout <<"cpu found: " << get_cpu_name_intrinsics() << std::endl;
+
+        return EXIT_SUCCESS;
+}
